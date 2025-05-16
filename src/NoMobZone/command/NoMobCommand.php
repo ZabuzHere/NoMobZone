8167771ab36a46512bc87c51ac1cd385fcc3b952
Devoo <fired9229@gmail.com>
2025-05-16 13:31:33 +0700

    permission fix

--- src/NoMobZone/command/NoMobCommand.php
+++ src/NoMobZone/command/NoMobCommand.php
@@ -5,48 +5,53 @@ namespace NoMobZone\command;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use pocketmine\command\utils\CommandException;
 use pocketmine\plugin\PluginOwned;
 use NoMobZone\Main;
 use NoMobZone\zone\ZoneManager;
 
 class NoMobCommand extends Command implements PluginOwned {
 
-	public function __construct() {
-		parent::__construct("nomob", "Set No-Mob Zone");
-	}
+    public function __construct() {
+        parent::__construct("nomob", "Set No-Mob Zone");
+        $this->setPermission("nomobzone.command");
+    }
 
-	public function execute(CommandSender $sender, string $commandLabel, array $args): void {
-		if (!$sender instanceof Player) {
-			$sender->sendMessage("Use this command in-game.");
-			return;
-		}
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("Use this command in the game.");
+            return;
+        }
 
-		switch (strtolower($args[0] ?? "")) {
-			case "pos1":
-				ZoneManager::setPos($sender->getName(), 1, $sender->getPosition());
-				$sender->sendMessage("Pos1 set.");
-				break;
+        if (!$this->testPermission($sender)) {
+            $sender->sendMessage("You do not have permission to run this command.");
+            return;
+        }
 
-			case "pos2":
-				ZoneManager::setPos($sender->getName(), 2, $sender->getPosition());
-				$sender->sendMessage("Pos2 set.");
-				break;
+        switch (strtolower($args[0] ?? "")) {
+            case "pos1":
+                ZoneManager::setPos($sender->getName(), 1, $sender->getPosition());
+                $sender->sendMessage("Post1 is marked.");
+                break;
 
-			case "end":
-				if (ZoneManager::confirmZone($sender->getName())) {
-					$sender->sendMessage("No-Mob Zone created!");
-				} else {
-					$sender->sendMessage("Set both pos1 and pos2 first.");
-				}
-				break;
+            case "pos2":
+                ZoneManager::setPos($sender->getName(), 2, $sender->getPosition());
+                $sender->sendMessage("Post2 is marked.");
+                break;
 
-			default:
-				$sender->sendMessage("/nomob pos1 | pos2 | end");
-		}
-	}
+            case "end":
+                if (ZoneManager::confirmZone($sender->getName())) {
+                    $sender->sendMessage("No-Mob Zone successfully created!");
+                } else {
+                    $sender->sendMessage("You have to set pos1 and pos2 first.");
+                }
+                break;
 
-	public function getOwningPlugin(): Main {
-		return Main::$instance;
-	}
+            default:
+                $sender->sendMessage("/nomob pos1 | pos2 | end");
+        }
+    }
+
+    public function getOwningPlugin(): Main {
+        return Main::$instance;
+    }
 }
